03e2e774203e162873a91fccb7716c8007d9f718
Michael A. Smith <msmith3@ebay.com>
2015-09-18 21:16:58 -0400

    Initial Structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Couchbase Zend Cache Backend
+
+Requires [php-couchbase](https://github.com/couchbaselabs/php-couchbase)
+
---
+++ src/lib/KojiroMike/Couchbase/Cache.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * A Zend_Cache_Backend for Couchbase, intended for Magento
+ * and largely inspired by Cm_Cache_Backend_Redis.
+ *
+ * @copyright Copyright (c) 2015 Michael A. Smith
+ * @license   http://opensource.org/licenses/MIT MIT License
+ * @author    Michael A. Smith
+ */
+class KojiroMike_Couchbase_Cache extends Zend_Cache_Backend implements Zend_Cache_Backend_ExtendedInterface
+{
+}
